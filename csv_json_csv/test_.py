cd1a97454e3e8a53ed8779dbe342432e7eddadbc
hikagestyle <65787033+hikagestyle@users.noreply.github.com>
2023-08-13 15:12:56 +0900

    Delete csv_json_csv directory

--- csv_json_csv/test_.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('output.json', 'r') as f:
-    data = json.load(f)
-
-with open('data.json', 'w') as f:
-    json.dump(data[10:25], f, indent=2, ensure_ascii=False)
-
-# python3 test_.py
